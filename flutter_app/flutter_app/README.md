937cc2eb4f7414d6f9820676921d1d7d8bae673b
浅墨兰 <344330607@edufree.win>
2020-03-04 18:00:58 +0800

    初始化项目

--- flutter_app/flutter_app/README.md
+++ flutter_app/flutter_app/README.md
@@ -1,4 +1,4 @@
-# hello_word
+# flutterapp
 
 A new Flutter application.
 
--- flutter_app/flutter_app/android/app/src/main/kotlin/com/mstains/flutterapp/MainActivity.kt
+++ flutter_app/flutter_app/android/app/src/main/kotlin/com/mstains/flutterapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mstains.hello_word
+package com.mstains.flutterapp
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
