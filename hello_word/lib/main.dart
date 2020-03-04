43a97b8741ab6d0cb214c917fd0cefeacb64c005
浅墨兰 <344330607@edufree.win>
2020-03-04 17:42:21 +0800

    1初始化项目

--- hello_word/lib/main.dart
+++ hello_word/lib/main.dart
@@ -1,157 +1,158 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyTheme());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Flutter"),
 
-        ),
-        body: Center(
-          child: Text("Hello Word"),
-        ),
+
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(20),
+      child: new Row(
+        children:[
+
+          new Expanded(
+              child: new Column(
+
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: <Widget>[
+
+                  new Container(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+
+                    child: new Text(
+                      'Oeschinen Lake Campground',
+                      style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                    ),
+                  ),
+                  new Text(
+                    'Kandersteg, Switzerland',
+                    style: new TextStyle(
+                      color: Colors.deepOrange,
+
+                    ),
+                  )
+
+                ],
+              ),
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41')
+        ],
+
       ),
     );
-  }
-}
 
 
-class MyTheme extends StatelessWidget{
 
-  @override
-  Widget build(BuildContext context) {
+    Column buildButtonColumn(IconData icon,String label){
 
-    final appName = "自定义主题";
+      Color color = Theme.of(context).primaryColor;
+
+      return new Column(
+
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: <Widget>[
+          new Icon(icon,color: color),
+          new Container(
+
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+
+              ),
+            ),
+
+          ),
+
+        ],
+
+
+      );
+    }
+
+
+
+    Widget buttonSection = new Container(
+
+      child: new Row(
+
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+        children: <Widget>[
+
+
+          buildButtonColumn(Icons.call, '电话'),
+          
+          buildButtonColumn(Icons.near_me, '定位'),
+
+          buildButtonColumn(Icons.share, '分享'),
+
+
+        ],
+
+      ),
+
+    );
+
+
+
+
+
+    Widget textSection = new Container(
+
+      padding: const EdgeInsets.all(32.0),
+
+      child: new Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,),
+    );
+
+
+
+
+
 
     return new MaterialApp(
 
-      title: appName,
-      theme: new ThemeData(
-
-        brightness: Brightness.light,
-
-        primaryColor: Colors.lightGreen[600],
-
-        accentColor: Colors.orange[600]
-
-      ),
-      home: new MyHelloWordHomePage(
-        title : appName
-      ),
-
-    );
-  }
-
-}
+      home: new Scaffold(
 
 
-class MyHomePage extends StatelessWidget {
-  final String title;
+        body: new ListView(
 
-  MyHomePage({Key key, @required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(title),
-
-      ),
-
-
-      body: new Center(
-        child: new Container(
-          color: Theme
-              .of(context)
-              .accentColor,
-          child: new Text(
-              '带有颜色背景的文本组件',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .title
-          ),
-        ),
-      ),
-      floatingActionButton: new Theme(
-          data: Theme.of(context).copyWith(accentColor: Colors.black),
-          child: new FloatingActionButton(
-            onPressed: null,
-            child: new Icon(Icons.add_location),)),
-    );
-  }
-
-
-}
-
-
-class MyHelloWordHomePage extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() => new _MyHelloWordHomePageState();
-
-  MyHelloWordHomePage({Key key,this.title}) : super(key:key);
-
-  final String title;
-
-}
-
-class _MyHelloWordHomePageState extends State<MyHelloWordHomePage>{
-
-  int _counter = 0;
-
-  void _addCounter(){
-    setState(() {
-
-      _counter++;
-
-    });
-
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-
-      //布局居中
-      body: new Center(
-        //垂直布局
-        child: new Column(
-          //主轴居中对齐
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
-            new Text('6666666'),
-            
-            new Text('哒哒哒'),
-
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            )
+//            new Image.asset("assets/images/bg.jpg",
+//              width: 600.0,
+//              height: 400.0,
+//              fit: BoxFit.cover,
+//
+//            ),
+            titleSection,
+            buttonSection,
+            textSection,
 
           ],
+
+
         ),
       ),
 
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _addCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
 
     );
+
   }
-
-
 }
