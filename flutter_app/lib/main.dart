04c4ebf06986f412440c5bf96bad44b6a5055818
浅墨兰 <344330607@edufree.win>
2020-03-06 12:04:47 +0800

    Container圆角

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -124,7 +124,7 @@ class MyApp extends StatelessWidget {
     );
 
     Widget columnSection = new Container(
-      margin: EdgeInsets.only(top: 10.0),
+      margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
       child: new Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
@@ -150,18 +150,107 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    Widget minSection = new Container(
+    Widget ratings = new Container(
       margin: EdgeInsets.only(top: 10.0),
       child: new Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          new Icon(Icons.star, color: Colors.green[500]),
-          new Icon(Icons.star, color: Colors.green[500]),
-          new Icon(Icons.star, color: Colors.green[500]),
-          new Icon(Icons.star, color: Colors.black),
-          new Icon(Icons.star, color: Colors.black),
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Icon(Icons.star, color: Colors.green[500]),
+              new Icon(Icons.star, color: Colors.green[500]),
+              new Icon(Icons.star, color: Colors.green[500]),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+            ],
+          ),
+          new Text(
+            '170 Reviews',
+            style: new TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20.0,
+            ),
+          )
         ],
+      ),
+    );
+
+    var descTextStyle = new TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18.0,
+      height: 2.0,
+    );
+
+    var iconList = DefaultTextStyle.merge(
+        style: descTextStyle,
+        child: new Container(
+          padding: EdgeInsets.all(20.0),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new Column(
+                children: <Widget>[
+                  new Icon(Icons.kitchen, color: Colors.green[500]),
+                  new Text('PREP:'),
+                  new Text('25 min'),
+                ],
+              ),
+              new Column(
+                children: <Widget>[
+                  new Icon(Icons.timer, color: Colors.green[500]),
+                  new Text('COOK:'),
+                  new Text('1 hr'),
+                ],
+              ),
+              new Column(
+                children: <Widget>[
+                  new Icon(Icons.restaurant, color: Colors.green[500]),
+                  new Text('FEEDS:'),
+                  new Text('4-6'),
+                ],
+              ),
+            ],
+          ),
+        ));
+
+    var leftColumn = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Icon(Icons.star, color: Colors.green[500]),
+              new Icon(Icons.star, color: Colors.green[500]),
+              new Icon(Icons.star, color: Colors.green[500]),
+              new Icon(Icons.star, color: Colors.black),
+              new Icon(Icons.star, color: Colors.black),
+            ],
+          ),
+          new Text('text')
+        ],
+      ),
+    );
+
+    var containerDecoration = new Container(
+      width: 400,
+      height: 100,
+      margin: EdgeInsets.all(16),
+      decoration: new BoxDecoration(
+        border: new Border.all(width: 1.0, color: Colors.red),
+        borderRadius: const BorderRadius.all(const Radius.circular(8.0)),
+      ),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[new Text('54334')],
       ),
     );
 
@@ -175,7 +264,8 @@ class MyApp extends StatelessWidget {
             textSection,
             rowSection,
             columnSection,
-            minSection,
+            leftColumn,
+            containerDecoration,
           ],
         ),
       ),
